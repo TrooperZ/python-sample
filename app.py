5d3193344ef2ceeed178425f29df691d37372af2
TrooperZ <trooperz.tz@gmail.com>
2021-07-02 19:56:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@
 # bot.py
 
 import discord
-import os
 from discord.ext import commands
+
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="!", intents=intents) #useless thing
+bot = commands.Bot(command_prefix="!!!!", intents=intents) #useless thing
 
 @bot.event
 async def on_ready():
@@ -15,14 +15,19 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    print(member.id)
     guild = bot.get_guild(860599727309717534) #tipcc trade, change id
     mainguild = bot.get_guild(860599706434666506) #tipcc, change id
 
+    if member.guild == mainguild:
+        return
+
+    logchannel = bot.get_channel(860599727309717538) #log here
+
     if mainguild.get_member(member.id) is not None:
-        print('This person is in tipcc')
+        await logchannel.send(f"{member} `{member.id}` joined the server, they are in the tip.cc main server, ignoring.")
     else:
-        await guild.ban(member)
-        print('Not in tipcc, banning')
+        await logchannel.send(f"{member} `{member.id}` joined the server, ***THEY ARE NOT IN THE TIP.CC MAIN SERVER***, banning due to suspicion")
+        await member.send("Your account has been banned from the tip.cc trading server because our system has picked the account up as a potential scammer/botter. To resolve this issue, go to discord.gg/tipcc and discuss it with one of the Server Moderators or Server Staff.")
+        await guild.ban(member, reason="Possible alt/bot/scammer detected by Tip.cc Tradekeeper")
 
 bot.run(os.getenv('DISCORD')) #bot token, do not disclose
