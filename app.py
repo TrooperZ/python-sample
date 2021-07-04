a35596593d28fe82f91de81d4383d72f1ab62d46
TrooperZ <trooperz.tz@gmail.com>
2021-07-04 08:56:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ async def tradeunban(ctx, user: discord.User):
         print(e)
         return await ctx.send("Unable to unban, maybe they aren't banned? (If they are banned, check bot logs)")
     await ctx.send(f"Unbanned {user.mention} from tip.cc trade server")
-    log = bot.get_channel(860599727309717538
+    log = bot.get_channel(860599727309717538) #set this to log channel id
     await log.send(f"{ctx.author} unbanned {user} from tip.cc trade server")
         
     
@@ -49,12 +49,15 @@ async def on_member_join(member):
         await logchannel.send(f"{member} `{member.id}` joined the server, they are in the tip.cc main server, ignoring.")
     else:
         await logchannel.send(f"{member} `{member.id}` joined the server, ***THEY ARE NOT IN THE TIP.CC MAIN SERVER***, banning due to suspicion")
-        await member.send("""You have been temporarily removed from the tip.cc trading server.
+        try:
+            await member.send("""You have been temporarily removed from the tip.cc trading server.
 
 The trading server is exclusive for Tip.cc server users because you are not a user of the mentioned server, we temporarily removed you from the tip.cc trading server.
 
 Once you have joined over at discord.gg/tipcc * please appeal your ban at https://discord.gg/afwSbGcEmN
 *You might be subject to further verification to be able to join tip.cc main server""")
+        except:
+            await logchannel.send("User has dms off, couldn't dm.")
         await guild.ban(member, reason="Possible alt/bot/scammer detected by Tip.cc Tradekeeper")
 
 bot.run(os.getenv('DISCORD')) #bot token, do not disclose
